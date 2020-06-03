a78c958c0877e224e215a335dff5487a7c8b7878
Krushan Bauva <krushan@bauva.com>
2020-06-03 16:42:37 +0530

    Complete problemset 0

---
+++ ps0/ps0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 20:29:00 2020
+
+@author: krushan
+"""
+
+import math
+
+x = int(input("Enter a number x: "))
+y = int(input("Enter a number y: "))
+
+# x = input("Enter a number x: ")
+# y = input("Enter a number y: ")
+
+print("x**y = ", x**y)
+print("log(x) = ", math.log(x, 2))
